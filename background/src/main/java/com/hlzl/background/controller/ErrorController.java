1b32f040906e34fb664a487bb3e34efa419d9115
赫连醉柳 <1273103690@qq.com>
2023-10-26 15:40:00 +0800

    add interface

--- background/src/main/java/com/hlzl/background/controller/ErrorController.java
+++ background/src/main/java/com/hlzl/background/controller/ErrorController.java
@@ -1,13 +1,11 @@
 package com.hlzl.background.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/error")
 public class ErrorController {
-    @GetMapping
+    @GetMapping("/error")
     public String getErrorMsg() {
         return "没有出错，你跑这干嘛";
     }
--- background/src/main/java/com/hlzl/background/controller/TestController.java
+++ background/src/main/java/com/hlzl/background/controller/TestController.java
@@ -1,20 +1,26 @@
 package com.hlzl.background.controller;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class TestController {
-    @GetMapping
+    @GetMapping("/testString")
     public String getTestString() {
-        return "Test String"; // TODO:
+        return "Test String";
+        // TODO:
     }
 
-    @PostMapping
-    public String PostMsg() {
-        return "123";
+    @PostMapping("/PostTest")
+    public HashMap<String, Object> PostMsg(@RequestParam String Name, Integer Age) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("Name", Name);
+        map.put("Age", Age);
+        return map;
     }
+
 }
